efe6a67a76069e04775d5dcc25d418f03ffd84c0
sebastian0023 <130592260+sebastian0023@users.noreply.github.com>
2024-11-24 22:46:40 -0600

    Update snake_ripes.c

--- snake_ripes.c
+++ snake_ripes.c
@@ -1,43 +1,62 @@
 #include "ripes_system.h"
-#include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-#define RED_COLOR 0xff0000
-#define GREEN_COLOR 0x00ff00
-#define BLACK_COLOR 0x000000
-#define BLUE_COLOR 0x0000ff
-#define INITIAL_SNAKE_SIZE 3;
-#define SNAKE_ALIVE 1
-#define SNAKE_DEAD 2
-#define SNAKE_WIN 3
+#define SW0 (0x01)
+#define SW1 (0x02)
+#define SW2 (0x04)
+#define SW3 (0x08)
+#define SW4 (0x10)
+#define SW5 (0x20)
+#define SW6 (0x40)
+#define SW7 (0x80)
 
-volatile unsigned int *led_base = LED_MATRIX_0_BASE;
-volatile unsigned int *pad_up = D_PAD_0_UP;
-volatile unsigned int *pad_down = D_PAD_0_DOWN;
-volatile unsigned int *pad_right = D_PAD_0_RIGHT;
-volatile unsigned int *pad_left = D_PAD_0_LEFT;
+// Constantes de tamaños
+#define LED_MATRIX_0_SIZE	(0xdac)
+#define LED_MATRIX_0_WIDTH	(0x23)
+#define LED_MATRIX_0_HEIGHT	(0x19)
 
-const int led_width = LED_MATRIX_0_WIDTH;
-const int led_height = LED_MATRIX_0_HEIGHT;
-const int led_size = LED_MATRIX_0_SIZE; 
+// Apuntadores a diferentes partes importantesm base y dpad
+volatile unsigned int * led_base = (volatile unsigned int *)LED_MATRIX_0_BASE;
 
-const int right_offset = 1;
-const int left_offset = -1;
-const int up_offset = -led_width;
-const int down_offset = led_width;
+volatile unsigned int * d_pad_up = (volatile unsigned int *)D_PAD_0_UP;
+volatile unsigned int * d_pad_do = (volatile unsigned int *)D_PAD_0_DOWN;
+volatile unsigned int * d_pad_le = (volatile unsigned int *)D_PAD_0_LEFT;
+volatile unsigned int * d_pad_ri = (volatile unsigned int *)D_PAD_0_RIGHT;
 
+// Tamaño de la matriz
+#define MAX_SNAKE_SIZE (LED_MATRIX_0_WIDTH * LED_MATRIX_0_HEIGHT)
 
-int rand_time = 50;
+// Variables
+int game = 1;
+int head = 1;
+int tail = 0;
 
+int randcounter = 50;
 
-int tail_index;
+int size = 2;
 
+int new_head_index;
 
-int *snake_parts[LED_MATRIX_0_WIDTH * LED_MATRIX_0_HEIGHT];
+int dx = 0;
+int dy = 1;
 
+// Arreglo donde se guardan las head y tails
+volatile unsigned int snakeLEDs[MAX_SNAKE_SIZE];
 
-int game_state;
+void initSnake() {
+    int st1 = (LED_MATRIX_0_WIDTH + 2);
+    int st2 = (LED_MATRIX_0_WIDTH * 2 + 2);
+    *(led_base + st1) = 0x00FF00; // Primer bloque de 2x4
+    *(led_base + st1 + 1) = 0x00FF00;
+    *(led_base + st1 + LED_MATRIX_0_WIDTH) = 0x00FF00;
+    *(led_base + st1 + LED_MATRIX_0_WIDTH + 1) = 0x00FF00;
 
+    *(led_base + st2) = 0x00FF00; // Segundo bloque de 2x4
+    *(led_base + st2 + 1) = 0x00FF00;
+    *(led_base + st2 + LED_MATRIX_0_WIDTH) = 0x00FF00;
+    *(led_base + st2 + LED_MATRIX_0_WIDTH + 1) = 0x00FF00;
 
-typedef enum _my_bool { false, true } boolean;
+    snakeLEDs[tail] = st1;
+    snakeLEDs[head] = st2;
+}
